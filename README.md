ff1562d2461c687d17619c94b716beefa36dfbdc
omovo <omovohtennison@gmail.com>
2024-03-01 19:54:01 +0000

    project completed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # weather-application
 Weather application using HTML,CSS, and JavaScript.
+
+
+apikey 4fe253a26a70341e62928d5760ef5d76
+
+https://api.openweathermap.org/data/2.5/weather?q=London&appid=4fe253a26a70341e62928d5760ef5d76
--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 let weather = {
-    "apikey": "6fc67abadd7f81dd1de5aebb7cf9ac00"
-}
+    "apikey": "4fe253a26a70341e62928d5760ef5d76",
+    fetchweather: function (city) {
+        fetch ( 
+            "https://api.openweathermap.org/data/2.5/weather?q="
+             + city 
+             + "&units=metric&appid=" 
+             + this.apikey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayweather(data));
+    },
+    displayweather: function(data) {
+        const { name } = data;
+        const {   icon, description  } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name.icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/"+ icon + "@2x.png"
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".temp").innerHTML = temp +  "°C";
+        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind speed: " + speed + "km/h";
+
+    } ,
+    search: function () {
+        this.fetchweather(document.querySelector(".search-input").value);
+    }
+};
+
+document
+.querySelector(".search button")
+.addEventListener("click" , function () {
+    weather.search();
+
+
+});
